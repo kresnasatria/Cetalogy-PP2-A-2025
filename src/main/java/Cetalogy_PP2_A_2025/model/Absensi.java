54840a641a23f4eb107b9d9f956a2dfc4438315b
Kresna <kresnasatria160@gmail.com>
2025-12-27 21:58:50 +0700

    Mengerjakan Model Absensi dan Mahasiswa

--- src/main/java/Cetalogy_PP2_A_2025/model/Absensi.java
+++ src/main/java/Cetalogy_PP2_A_2025/model/Absensi.java
@@ -1,6 +1,55 @@
 package Cetalogy_PP2_A_2025.model;
 
+import java.sql.Date;
 
 public class Absensi {
-    
+
+    private int id;
+    private String nim;
+    private Date tanggal;
+    private String status;
+
+    // Constructor kosong
+    public Absensi() {}
+
+    // Constructor dengan parameter
+    public Absensi(int id, String nim, Date tanggal, String status) {
+        this.id = id;
+        this.nim = nim;
+        this.tanggal = tanggal;
+        this.status = status;
+    }
+
+    // Getter & Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public Date getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(Date tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
