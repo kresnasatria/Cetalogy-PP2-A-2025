5cb8ffda7bc27a9cb77d48539c1ee6fb9a7e529d
Kresna <kresnasatria160@gmail.com>
2025-12-28 12:47:44 +0700

    Mengerjakan Main

--- src/main/java/Cetalogy_PP2_A_2025/main/Main.java
+++ src/main/java/Cetalogy_PP2_A_2025/main/Main.java
@@ -1,6 +1,9 @@
 package Cetalogy_PP2_A_2025.main;
 
+import Cetalogy_PP2_A_2025.view.Dashboard;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        new Dashboard().setVisible(true);
+    }
 }
